22ac30929d79bc8f35d08b95e1d89dce4cdebab1
Emmanuels-code <arocket04@gmail.com>
2024-12-31 04:33:34 +0100

    fix: fixing image to link previews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,14 +10,16 @@ export const metadata: Metadata = {
     openGraph: {
         title: 'New Year Countdown',
         description: 'Countdown to the New Year!',
-        images: ['icon.png']
+        images: [{ url: 'https://new-year-countdown-eight-lilac.vercel.app/icon.png' }]
 
     },
     twitter: {
         card: 'summary_large_image',
         title: 'Twitter New Year Countdown',
         description: 'Countdown to the New Year!',
-        images: ['icon.png']
+        images: 'https://new-year-countdown-eight-lilac.vercel.app/icon.png',
+        creator: "@randomc10215341"
+
 
 
     },
