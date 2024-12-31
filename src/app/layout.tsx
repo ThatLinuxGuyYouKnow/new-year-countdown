24af136069898e9bb0f8391bf3cad3e3a97b023d
Emmanuels-code <arocket04@gmail.com>
2024-12-31 12:00:01 +0100

    attempted tag fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,12 @@ export const metadata: Metadata = {
     openGraph: {
         title: 'New Year Countdown',
         description: 'Countdown to the New Year!',
-        images: [{ url: 'https://new-year-countdown-eight-lilac.vercel.app/icon.png' }]
+        images: 'https://new-year-countdown-eight-lilac.vercel.app/icon.png',
 
     },
     twitter: {
         card: 'summary_large_image',
-        title: 'Twitter New Year Countdown',
+        title: 'New Year Countdown',
         description: 'Countdown to the New Year!',
         images: 'https://new-year-countdown-eight-lilac.vercel.app/icon.png',
         creator: "@randomc10215341"
