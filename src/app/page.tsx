ee690495ad3b91b1648d994eebdab56dc96d3133
Emmanuels-code <arocket04@gmail.com>
2024-12-31 02:55:10 +0100

    test:trimmed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,4 @@
-import { Metadata } from 'next'
-import { headers } from 'next/headers'
 import CountdownPage from './countdown-page'
-import { formatCountdown } from './utils/format-countdown'
-
-export const runtime = 'edge'
-
-export async function generateMetadata(): Promise<Metadata> {
-  const host = (await headers()).get('host')
-  const protocol = process?.env.NODE_ENV === 'development' ? 'http' : 'https'
-  const timeLeft = getTimeLeft()
-  const { days, hours, minutes, seconds } = formatCountdown(timeLeft)
-  const countdownString = `${days}:${hours}:${minutes}:${seconds}`
-
-  return {
-    title: 'New Year Countdown',
-    description: 'Countdown to the New Year!',
-    openGraph: {
-      title: 'New Year Countdown',
-      description: 'Countdown to the New Year!',
-      images: [`${protocol}://${host}/api/og?countdown=${countdownString}`],
-    },
-    twitter: {
-      card: 'summary_large_image',
-    },
-  }
-}
-
-function getTimeLeft(): number {
-  const now = new Date()
-  const currentYear = now.getFullYear()
-  const nextYear = new Date(currentYear + 1, 0, 1)
-  return nextYear.getTime() - now.getTime()
-}
 
 export default function Page() {
   return <CountdownPage />
